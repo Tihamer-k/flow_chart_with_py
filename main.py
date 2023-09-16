02b6fef9788f0564faf09d59189cd1f961dd95c2
tihamerkarlovich.aldana.contractor <tihamerkarlovich.aldana.contractor@bbva.com>
2023-09-16 16:20:52 -0500

    Add VCS mapping and improve readability of scripts
    A new VCS mapping file was created for Git to simplify version control integration. The instructions on `main.py` and `process_op.py` were improved for better readability, by adding comments for explanation and standardizing user prompts and print statements to have consistent capitalization.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
-from html2image import Html2Image
-from py2flowchart import *
-from PIL import Image
+from py2flowchart import *  # lib para convertir un archivo PY a HTML
+from html2image import Html2Image  # lib para convertir el HTML  a imagen
+from PIL import Image  # lib para abrir la imagen creada
 
-current_path = os.getcwd()
+current_path = os.getcwd()  # para obtener la ruta actual del proyecto
 path_project = (current_path + '\\process_op.py')
 
 
